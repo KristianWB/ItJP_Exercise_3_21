27fc9a5880e095526237d141ca1115af3a26e85c
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-01-30 11:25:54 +0100

    Initial commit

---
+++ src/DayOfTheWeek.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class DayOfTheWeek {
+    public static void main(String[] args)   {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print(
+                "Enter the year: "
+        );
+        int year = input.nextInt();
+
+        System.out.print(
+                "Enter the month from 1 - 12: Where Marts is 3 and december is 12, january and february is 1 nd 2 of the previous: "
+        );
+        int m = input.nextInt();
+
+        System.out.print(
+                "Enter the day of the month: "
+        );
+        int q = input.nextInt();
+
+        int k = year%100;
+
+        int j = year/100;
+
+        int h = (q + (26*(m+1)/10) + k + k/4 + j/4 + 5*j)%7;
+
+        System.out.print(h+"\n");
+
+        switch(h)   {
+            case 0: System.out.print(
+                    "The day is Saturday"
+            );
+            break;
+            case 1: System.out.print(
+                    "The day is Sunday"
+            );
+            break;
+            case 2: System.out.print(
+                    "The day is Monday"
+            );
+            break;
+            case 3: System.out.print(
+                    "The day is Tuesday"
+            );
+            break;
+            case 4: System.out.print(
+                    "The day is Wednesday"
+            );
+            break;
+            case 5: System.out.print(
+                    "The day is Thursday"
+            );
+            break;
+            case 6: System.out.print(
+                    "The day is Friday"
+            );
+            System.exit(1);
+        }
+    }
+}
